7e392cf7c3225e05f83a50767aec5bfa82dd0012
Pascal Brand <pascal.brand38@gmail.com>
2024-01-08 21:30:44 +0100

    hardcoded buildhHtml of ../web-portfolio
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ npm install
 This will install
 * ```npm install gulp-cli```: the gulp commandline
 * ```npm install gulp```: to be able to run tasks, such as ```gulp helloworld```
+* ```npm install gulp-rename```:
+  rename a stream, so that the destination has a different name than the source
+  (```.hbs``` to ```.html``` for example)
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,34 @@
-// run helloworld taksk using:  gulp helloworld
+// Copyright (c) Pascal Brand
+// MIT License
+
+const gulp = require('gulp');
+const { series, parallel } = require('gulp');
+const gulprename = require("gulp-rename");
+
+
 function helloworldTask(cb) {
   console.log('Hello World')
-  cb();
+  cb()
 }
 
+function buildHtmlTask(done) {
+  src = '../web-portfolio/src/hbs/*.hbs'
+  dst = '../web-portfolio/dist'
+  return gulp.src(src)
+    .pipe(gulprename(function (path) {
+      // Updates the object in-place
+      //path.dirname += "/ciao";
+      //path.basename += "-goodbye";
+      path.extname = ".html";
+    }))
+    .pipe(gulp.dest(dst))
+}
+
+///////////////////////// Tasks
+// run helloworld task using:  gulp helloworld
 exports.helloworld = helloworldTask
+exports.buildHtml = buildHtmlTask
+
+exports.default = series(
+  buildHtmlTask,
+)
