c985eee7dea13f1f5c29e1f57f73ad294ead9226
Pascal Brand <pascal.brand38@gmail.com>
2024-01-09 15:32:29 +0100

    gulp-config.json for private variables
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
 // Copyright (c) Pascal Brand
 // MIT License
 
+const fs = require('fs')
 const gulp = require('gulp');
 const { series, parallel } = require('gulp');
-const { buildHtml } = require('./js/buildHtml.js')
+const { buildHtml } = require('./js/buildHtml')
+const { createPreprocVariables } = require('./js/preproc')
+
+
+////////////////////////////////////////////////////////////////////// Initialization
 
 console.log(buildHtml)
 
 // TODO: remove these hard values
 const args = {
-  now: new Date(),
   siteRootdir: '../web-portfolio',
   relativeDst: 'dist',
 }
+
+async function initTask() {
+  args.gulpConfig = await JSON.parse(fs.readFileSync(args.siteRootdir + '/gulp-config.json', 'utf8'))
+  createPreprocVariables(args)
+  console.log(args)
+}
+
+
+////////////////////////////////////////////////////////////////////// Tasks
 
 function helloworldTask(cb) {
   console.log('Hello World')
@@ -26,5 +39,6 @@ const buildHtmlTask = (cb) => buildHtml(args, cb)
 exports.helloworld = helloworldTask
 
 exports.default = series(
+  initTask,
   buildHtmlTask,
 )
--- js/preproc.js
+++ js/preproc.js
@@ -5,25 +5,30 @@ const gulphandlebars = require('gulp-hb');
 
 const _digits2 = (number) => number < 10 ? '0' + number : '' + number
 
-function preproc(args, cb) {
-  const epoch = Math.round(args.now.getTime() / 1000);
+function createPreprocVariables(args) {
+  const now = new Date()
+  const epoch = Math.round(now.getTime() / 1000);
 
-  const data = {
+  args.preprocVariables = {
     // time and date
     WEBTOOLS_FILE_VERSION: 'v=' + epoch,
     WEBTOOLS_EPOCH: epoch,
-    WEBTOOLS_SECOND: _digits2(args.now.getSeconds()),
-    WEBTOOLS_MINUTE: _digits2(args.now.getMinutes()),
-    WEBTOOLS_HOUR: _digits2(args.now.getHours()),
-    WEBTOOLS_YEAR: args.now.getFullYear(),
-    WEBTOOLS_MONTH: _digits2(args.now.getMonth()+1),
-    WEBTOOLS_DAY: _digits2(args.now.getDate()),
-  }
-  return gulphandlebars()
-      //.partials('C:/msys64/home/pasca/dev/other/web-design/site/*/dev/partial/*.html')
-      // .partials('site/*/dev/partial/*.hbs')
-      .data(data)
+    WEBTOOLS_SECOND: _digits2(now.getSeconds()),
+    WEBTOOLS_MINUTE: _digits2(now.getMinutes()),
+    WEBTOOLS_HOUR: _digits2(now.getHours()),
+    WEBTOOLS_YEAR: now.getFullYear(),
+    WEBTOOLS_MONTH: _digits2(now.getMonth()+1),
+    WEBTOOLS_DAY: _digits2(now.getDate()),
 
+    // adding private preproc variables
+    ...args.gulpConfig.preprocVariables,
+  }
+}
+
+function preproc(args, cb) {
+  return gulphandlebars()
+      .data(args.preprocVariables)
 }
 
 exports.preproc = preproc
+exports.createPreprocVariables = createPreprocVariables
