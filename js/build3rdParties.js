a396b930e67ecc17e6849e68903bef3782d11c78
Pascal Brand <pascal.brand38@gmail.com>
2024-02-12 17:03:41 +0100

    build3rdParties.js: export css
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- js/build3rdParties.js
+++ js/build3rdParties.js
@@ -2,10 +2,17 @@
 // MIT License
 
 const gulp = require('gulp');
+const gulprename = require('gulp-rename');
+
+function folder(file) {
+  console.log(file)
+  return undefined
+}
 
 function build3rdParties(args, done) {
-  return gulp.src('webtools/3rdparties/**/*.js', {"allowEmpty": true})
-    .pipe(gulp.dest(args.siteRootdir + '/' + args.relativeDst + '/js'))
+  return gulp.src('webtools/3rdparties/**/*.{js,css}', {"allowEmpty": true})
+    .pipe(gulprename(function (path) { path.dirname = path.extname.slice(start=1); }))
+    .pipe(gulp.dest(args.siteRootdir + '/' + args.relativeDst))
 }
 
 exports.build3rdParties = build3rdParties;
