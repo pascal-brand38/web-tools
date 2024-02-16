6648cc87fa205381f1e75706183789fd4cfa0139
Pascal Brand <pascal.brand38@gmail.com>
2024-02-16 20:24:10 +0100

    buildImg: parallel, and async function
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- js/buildImg.js
+++ js/buildImg.js
@@ -2,78 +2,48 @@
 // MIT License
 
 const fs = require('fs')
-const child_process = require('child_process');
-const gulp = require('gulp');
 
-function runpython(list_args, cb) {
-  var python = child_process.spawn('python', list_args);
+// https://stackoverflow.com/questions/30763496/how-to-promisify-nodes-child-process-exec-and-child-process-execfile-functions
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
-  python.stdout.on('data', function (data) {
-    console.log(data.toString());
-  });
-
-  python.stderr.on('data', function (data) {
-    console.log(data.toString());
-  });
-
-  python.on('exit', function (code) {
-    if (code == 0) {
-      cb();
-    } else {
-      cb(new Error("Failure"))
-    }
-  });
+async function runcommand(command) {
+  try {
+    const { stdout, stderr } = await exec(command);
+    console.log(stdout);
+    console.log(stderr);
+  } catch (e) {
+    console.error(e); // should contain code (exit code) and signal (that caused the termination).
+  }
 }
 
-function buildSprite(args, done) {
+async function buildSprite(args) {
   const spriteName = args.siteRootdir + '/src/img/sprite/sprite.json'
   if (!fs.existsSync(spriteName)) {
     console.log(`Sprite file ${spriteName} does not exist`)
-    done()
   } else {
-    runpython([
-      '-m',
-      'spriteforhtml',
-      '--json',
-      spriteName
-    ], done)
+    await runcommand(`python -m spriteforhtml --json ${spriteName}`)
   }
 }
 
-function buildImgLogo(args, done) {
+async function buildImgLogo(args) {
   const srcName = args.siteRootdir + '/src/img/logo'
   if (!fs.existsSync(srcName)) {
     console.log(`Dir name ${srcName} does not exist`)
-    done()
   } else {
-    runpython([
-      '-m',
-      'responsiveimage',
-      '--src-dir', srcName,
-      '--dst-dir', args.siteRootdir + '/' + args.relativeDst + '/img',
-      '--export-to-webp',
-    ], done)
+    await runcommand(`python -m responsiveimage --src-dir ${srcName} --dst-dir ${args.siteRootdir + '/' + args.relativeDst + '/img'} --export-to-webp`)
   }
 }
 
 
-function buildImg(args, done) {
+async function buildImg(args, done) {
   const { buildLocalImg } = require('../' + args.siteRootdir + '/src/gulp-config/buildLocal')
 
-  const funcs = [ buildImgLogo, buildSprite, buildLocalImg ]
-  let nbdone = 0;
-  function completeTask(e) {
-    if (e) {
-      throw e
-    } else {
-      nbdone ++;
-      if (nbdone === funcs.length) {
-        done()
-      }
-    }
-  }
-
-  funcs.forEach(func => func(args, completeTask))
+  await Promise.all([
+    buildImgLogo(args),
+    buildSprite(args),
+    buildLocalImg(args),
+    ])
 }
 
 exports.buildImg = buildImg
