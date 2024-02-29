30b4c25e34779629235b3041c56912fff0afca4b
Pascal Brand <pascal.brand38@gmail.com>
2024-02-29 17:26:19 +0100

    js/buildPhp.js: preproc php
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- js/buildPhp.js
+++ js/buildPhp.js
@@ -3,12 +3,16 @@
 
 const gulp = require('gulp');
 const gulptap = require('gulp-tap');
+const { preprocHandlebars } = require('./preproc')
 
 function buildPhp(args, done) {
   return gulp.src(args.siteRootdir + '/src/php/*', {"allowEmpty": true})
     .pipe(gulptap(function (file, t) {
-      console.log(file.basename)
+      return preprocHandlebars(args, file)
     }))
+    // .pipe(gulptap(function (file, t) {
+    //   console.log(file.basename)
+    // }))
     .pipe(gulp.dest(args.siteRootdir + '/' + args.relativeDst + '/php'))
 }
 
